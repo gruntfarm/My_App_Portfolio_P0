6e39f4e7f259f3fa2bd0e654868257d9410cd976
Mladen Kovacevic <mladen.kovacevic@gmail.com>
2015-06-17 22:53:48 -0400

    new file:   MyAppPortfolio.iml 	new file:   app/.gitignore 	new file:   app/app.iml 	new file:   app/build.gradle 	new file:   app/proguard-rules.pro 	new file:   app/src/androidTest/java/com/example/android/myappportfolio/ApplicationTest.java 	new file:   app/src/main/AndroidManifest.xml 	new file:   app/src/main/java/com/example/android/myappportfolio/.gitignore 	new file:   app/src/main/java/com/example/android/myappportfolio/MainActivity.java 	new file:   app/src/main/res/layout/activity_main.xml 	new file:   app/src/main/res/mipmap-hdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-mdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xhdpi/ic_launcher.png 	new file:   app/src/main/res/mipmap-xxhdpi/ic_launcher.png 	new file:   app/src/main/res/values-w820dp/dimens.xml 	new file:   app/src/main/res/values/dimens.xml 	new file:   app/src/main/res/values/strings.xml 	new file:   app/src/main/res/values/styles.xml 	new file:   build.gradle 	new file:   settings.gradle

---
+++ app/src/main/java/com/example/android/myappportfolio/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.android.myappportfolio;
+
+import android.os.Bundle;
+import android.content.Context;
+import android.support.v4.app.FragmentActivity;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.View;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        //int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+    public void sendMessage(View view) {
+        Button button = (Button) view;
+        String buttonText = (String) button.getTag();
+        Context context = getApplicationContext();
+        String message = getString(R.string.launches) + " " + buttonText;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, message, duration);
+        toast.show();
+    }
+}
+
